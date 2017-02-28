9c47f0578aa2bccdcd4c416dcb4d2595bb8b9e6a
Josh Fuchs <fuchs.josh@gmail.com>
2017-02-28 10:47:05 -0500

    Option to run LA COSMIC

--- reduction.py
+++ reduction.py
@@ -14,8 +14,9 @@ import numpy as np
 import ReduceSpec_tools as rt
 import spectools as st
 import warnings
+import pyfits as fits
 
-task = raw_input('What would you like to do? (bias, flat, normalize, combine, trim, wavelength) ')
+task = raw_input('What would you like to do? (bias, flat, normalize, lacosmic, combine, trim, wavelength, details) ')
 
 if task == 'combine':
     files = raw_input('Name of file containing images to combine: ')
@@ -31,6 +32,12 @@ if task == 'combine':
 if task == 'trim':
     files = raw_input('Name of file to trim: ')
     rt.Trim_Spec(files)
+
+if task == 'lacosmic':
+    files = raw_input('Name of file containing images to combine: ')
+    filelist = rt.Read_List(files)
+    for x in filelist:
+        rt.lacosmic(x)
 
 if task == 'wavelength':
     Wavefile = raw_input('Name of file with wavelength solution: ')
@@ -57,4 +64,10 @@ if task == 'flat':
 
     rt.Flat_Field(filelist,flatfile)
 
-    
+if task == 'details':
+    files = raw_input('Name of file containing images to combine: ')
+    filelist = np.genfromtxt(files,dtype=str)
+    for x in filelist:
+        hdulist = fits.open(x)
+        hdu = hdulist[0]
+        print x, hdu.header['ADCSTAT'], hdu.header['EXPTIME'], hdu.header['SLIT'], hdu.header['GRATING']
